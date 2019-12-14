8c91635dff76595ccda3af0402ca1f315e7a4066
NicolasLucio <54450123+NicolasLucio@users.noreply.github.com>
2019-12-14 14:21:31 -0300

    Pause button included

--- Assets/Scripts/PrimarySystem.cs
+++ Assets/Scripts/PrimarySystem.cs
@@ -10,17 +10,43 @@ public class PrimarySystem : MonoBehaviour
 
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI snakeGameText;
 
     private int timerInSeconds;
+    private bool paused;
 
     void Start()
     {
+        paused = false;
         timerInSeconds = 0;
         snakeScript = snakeObject.GetComponent<SnakeControl>();
         timerText.text = timerInSeconds.ToString();
         StartCoroutine(SystemTimer());
     }
-    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {            
+            if (paused == false)
+            {
+                Time.timeScale = 0.0f;
+                paused = true;
+                snakeGameText.text = "PAUSED";
+            }
+            else if (paused == true)
+            {
+                Time.timeScale = 1.0f;
+                paused = false;
+                snakeGameText.text = "SNAKE GAME";
+            }            
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
     void FixedUpdate()
     {
         scoreText.text = "Score: " + snakeScript.scoreNumber.ToString();
